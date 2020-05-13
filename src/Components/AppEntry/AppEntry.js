9f502c164656ec2e62e11f78d3f4e47cbdea2e8e
Ahsan Ullah <ahsanullah.au@gmail.com>
2020-05-13 16:24:14 -0400

    Made default date for updating current entries

--- src/Components/AppEntry/AppEntry.js
+++ src/Components/AppEntry/AppEntry.js
@@ -4,7 +4,6 @@ const AppEntry = ({ appID, appCompany, appRole, appLocation, appDate, appRespons
 
     const updateApplication = () => {
         if (appID && newApplication.newCompany && newApplication.newRole && newApplication.newLocation && newApplication.newDate && newApplication.newResponse && newApplication.newLink) {
-            console.log(1)
             fetch('http://localhost:3001/applications', {
                 method: 'put',
                 headers: { 'Content-Type': 'application/json' },
@@ -84,7 +83,7 @@ const AppEntry = ({ appID, appCompany, appRole, appLocation, appDate, appRespons
                 <td className="pa3"><input type="text" id="AddCompany" defaultValue={appCompany} onChange={(evt) => { setNewApplication({ ...newApplication, newCompany: evt.target.value }) }} /></td>
                 <td className="pa3"><input type="text" id="AddRole" defaultValue={appRole} onChange={(evt) => { setNewApplication({ ...newApplication, newRole: evt.target.value }) }} /></td>
                 <td className="pa3"><input type="text" id="AddLocation" defaultValue={appLocation} onChange={(evt) => { setNewApplication({ ...newApplication, newLocation: evt.target.value }) }} /></td>
-                <td className="pa3"><input type="date" id="AddDate" defaultValue={appDate} onChange={(evt) => { setNewApplication({ ...newApplication, newDate: evt.target.value }) }} /></td>
+                <td className="pa3"><input type="date" id="AddDate" defaultValue={appDate.substring(0,10)} onChange={(evt) => { setNewApplication({ ...newApplication, newDate: evt.target.value }) }} /></td>
                 <td className="pa3"><input type="text" id="AddResponse" defaultValue={appResponse} onChange={(evt) => { setNewApplication({ ...newApplication, newResponse: evt.target.value }) }} /></td>
                 <td className="pa3"><input type="text" id="AddLink" defaultValue={appLink} onChange={(evt) => { setNewApplication({ ...newApplication, newLink: evt.target.value }) }} /></td>
                 <td className="pa3"><input type="text" id="AddNotes" defaultValue={appNotes} onChange={(evt) => { setNewApplication({ ...newApplication, newNotes: evt.target.value }) }} /></td>
