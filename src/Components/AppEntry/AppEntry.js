cfa9245f6cba212c9d1c3afa1d3e43ff2a9b24d0
Ahsan Ullah <ahsanullah.au@gmail.com>
2020-05-13 13:29:30 -0400

    Update now working, with default values

--- src/Components/AppEntry/AppEntry.js
+++ src/Components/AppEntry/AppEntry.js
@@ -3,6 +3,32 @@ import React, { useState } from 'react';
 const AppEntry = ({ appID, appCompany, appRole, appLocation, appDate, appResponse, appLink, appNotes, getApplications }) => {
 
     const updateApplication = () => {
+        if (appID && newApplication.newCompany && newApplication.newRole && newApplication.newLocation && newApplication.newDate && newApplication.newResponse && newApplication.newLink) {
+            console.log(1)
+            fetch('http://localhost:3001/applications', {
+                method: 'put',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    appID: appID,
+                    appCompany: newApplication.newCompany,
+                    appRole: newApplication.newRole,
+                    appLocation: newApplication.newLocation,
+                    appDate: newApplication.newDate,
+                    appResponse: newApplication.newResponse,
+                    appLink: newApplication.newLink,
+                    appNotes: newApplication.newNotes
+
+                })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    setModifyState(0)
+                    getApplications();
+                })
+
+        }
+
+
 
     }
 
@@ -18,9 +44,7 @@ const AppEntry = ({ appID, appCompany, appRole, appLocation, appDate, appRespons
                 })
             })
                 .then(response => response.json())
-                .then(data => {
-                    getApplications();
-                })
+                .then(getApplications())
 
 
 
@@ -28,46 +52,47 @@ const AppEntry = ({ appID, appCompany, appRole, appLocation, appDate, appRespons
 
     }
 
-    const [newApplication, setNewApplication] = useState([])
+    const [newApplication, setNewApplication] = useState({
+        newCompany: appCompany,
+        newRole: appRole,
+        newLocation: appLocation,
+        newDate: appDate,
+        newResponse: appResponse,
+        newLink: appLink,
+        newNotes: appNotes
+    })
 
     const [modifyState, setModifyState] = useState(0)
 
-    const ChangeModify = () => {
-        if (modifyState === 0) {
-            return (
-                <tr className="stripe-dark w-100">
-                    <td className="pa3">{appCompany}</td>
-                    <td className="pa3">{appRole}</td>
-                    <td className="pa3">{appLocation}</td>
-                    <td className="pa3">{appDate.substring(0, 10)}</td>
-                    <td className="pa3">{appResponse}</td>
-                    <td className="pa3">{appLink}</td>
-                    <td className="pa3">{appNotes}</td>
-                    <td className="pa1"><button id={"EditApplication" + appID} value="Edit" onClick={() => setModifyState(1)}>Edit</button></td>
-                    <td className="pa1"><button id={"DeleteApplication" + appID} value="Delete" onClick={deleteApplication}>Delete</button></td>
-                </tr>)
-        }
-        else {
-            return (
-                <tr className="stripe-dark w-100">
-                    <td className="pa3"><input type="text" id="AddCompany" onChange={(evt) => { setNewApplication({ ...newApplication, newCompany: evt.target.value }) }} /></td>
-                    <td className="pa3"><input type="text" id="AddRole" onChange={(evt) => { setNewApplication({ ...newApplication, newRole: evt.target.value }) }} /></td>
-                    <td className="pa3"><input type="text" id="AddLocation" onChange={(evt) => { setNewApplication({ ...newApplication, newLocation: evt.target.value }) }} /></td>
-                    <td className="pa3"><input type="date" id="AddDate" onChange={(evt) => { setNewApplication({ ...newApplication, newDate: evt.target.value }) }} /></td>
-                    <td className="pa3"><input type="text" id="AddResponse" onChange={(evt) => { setNewApplication({ ...newApplication, newResponse: evt.target.value }) }} /></td>
-                    <td className="pa3"><input type="text" id="AddLink" onChange={(evt) => { setNewApplication({ ...newApplication, newLink: evt.target.value }) }} /></td>
-                    <td className="pa3"><input type="text" id="AddNotes" onChange={(evt) => { setNewApplication({ ...newApplication, newNotes: evt.target.value }) }} /></td>
-                    <td className="pa1"><button id="AddApplication" value="Add" onClick={() => setModifyState(0)} >Cancel</button></td>
-                    <td className="pa1"></td>
-                </tr>)
-
-        }
-    
+    if (modifyState === 0) {
+        return (
+            <tr className="stripe-dark w-100">
+                <td className="pa3">{appCompany}</td>
+                <td className="pa3">{appRole}</td>
+                <td className="pa3">{appLocation}</td>
+                <td className="pa3">{appDate.substring(0, 10)}</td>
+                <td className="pa3">{appResponse}</td>
+                <td className="pa3">{appLink}</td>
+                <td className="pa3">{appNotes}</td>
+                <td className="pa1"><button id={"EditApplication" + appID} value="Edit" onClick={() => setModifyState(1)}>Edit</button></td>
+                <td className="pa1"><button id={"DeleteApplication" + appID} value="Delete" onClick={deleteApplication}>Delete</button></td>
+            </tr>)
     }
+    else {
+        return (
+            <tr className="stripe-dark w-100">
+                <td className="pa3"><input type="text" id="AddCompany" defaultValue={appCompany} onChange={(evt) => { setNewApplication({ ...newApplication, newCompany: evt.target.value }) }} /></td>
+                <td className="pa3"><input type="text" id="AddRole" defaultValue={appRole} onChange={(evt) => { setNewApplication({ ...newApplication, newRole: evt.target.value }) }} /></td>
+                <td className="pa3"><input type="text" id="AddLocation" defaultValue={appLocation} onChange={(evt) => { setNewApplication({ ...newApplication, newLocation: evt.target.value }) }} /></td>
+                <td className="pa3"><input type="date" id="AddDate" defaultValue={appDate} onChange={(evt) => { setNewApplication({ ...newApplication, newDate: evt.target.value }) }} /></td>
+                <td className="pa3"><input type="text" id="AddResponse" defaultValue={appResponse} onChange={(evt) => { setNewApplication({ ...newApplication, newResponse: evt.target.value }) }} /></td>
+                <td className="pa3"><input type="text" id="AddLink" defaultValue={appLink} onChange={(evt) => { setNewApplication({ ...newApplication, newLink: evt.target.value }) }} /></td>
+                <td className="pa3"><input type="text" id="AddNotes" defaultValue={appNotes} onChange={(evt) => { setNewApplication({ ...newApplication, newNotes: evt.target.value }) }} /></td>
+                <td className="pa1"><button id="AddUpdatedApplication" value="Submit" onClick={() => updateApplication()} >Update</button></td>
+                <td className="pa1"><button id="CancelUpdatedApplication" value="Cancel" onClick={() => setModifyState(0)} >Cancel</button></td>
 
-return (
-    <ChangeModify />
-)
+            </tr>)
+    }
 }
 
 export default AppEntry;
