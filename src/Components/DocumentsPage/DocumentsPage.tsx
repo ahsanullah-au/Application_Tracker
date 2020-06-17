322328ca9a36df58b638f189653d454b44e51644
Ahsan Ullah <ahsanullah.au@gmail.com>
2020-06-17 17:27:49 -0400

    Base upload element made

--- src/Components/DocumentsPage/DocumentsPage.tsx
+++ src/Components/DocumentsPage/DocumentsPage.tsx
@@ -9,19 +9,22 @@ interface DocPageType {
     setUserDocs: Function
 }
 
+const [uploadState, setUploadState] = useState({success: false, url:""})
+
+const handleUpload = ()  => {
+
+}
+
 const DocumentsPage = ({ user, setRoute, userDocs, setUserDocs }: DocPageType) => {
     return (
         <>
             <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <p
-                    onClick={() => {
-                        setRoute('Home');
-                    }}
-                    className="f2 link dim black underline pa3 pointer"
-                >
-                    Go Back
-          </p>
+                <p onClick={() => { setRoute('Home') }} className="f2 link dim black underline pa3 pointer">Go Back</p>
             </nav>
+            <h1>Upload</h1>
+            {uploadState.success? <h1>Successful Upload</h1>: null}
+            <input onChange={handleUpload} type="file"/>
+
         </>
     )
 }
