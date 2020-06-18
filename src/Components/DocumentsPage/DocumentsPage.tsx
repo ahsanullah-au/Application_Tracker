81dee01d0f9c3d50602d1ae93d1e20117af49ebd
Ahsan Ullah <ahsanullah.au@gmail.com>
2020-06-18 17:40:16 -0400

    Frontend adds files to s3 bucket using presignedURL

--- src/Components/DocumentsPage/DocumentsPage.tsx
+++ src/Components/DocumentsPage/DocumentsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 
 import type { userType, userDocsArrayType } from '../../App';
 
@@ -9,21 +9,57 @@ interface DocPageType {
     setUserDocs: Function
 }
 
-const [uploadState, setUploadState] = useState({success: false, url:""})
-
-const handleUpload = ()  => {
-
+interface UploadStateType {
+    success: boolean,
+    url: string,
+    file: null | FileList
 }
 
+
 const DocumentsPage = ({ user, setRoute, userDocs, setUserDocs }: DocPageType) => {
+
+    const [uploadState, setUploadState] = useState<UploadStateType>({ success: false, url: "", file: null })
+
+    const handleUploadChange = (evt: ChangeEvent) => {
+        setUploadState({ success: false, url: "", file: (evt.target as HTMLInputElement).files })
+    }
+
+    const handleUpload = () => {
+        if (uploadState.file) {
+            const file = uploadState.file[0]
+            console.log("Prep")
+            fetch('http://localhost:3001/docStorage', {
+                method: 'POST', // or 'PUT'
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user: user.id,
+                    fileName: file.name.split('.')[0],
+                    fileType: file.name.split('.')[1]
+                }),
+            })
+                .then(data => data.json())
+                .then(data => {
+                    fetch(data.signedRequest, { method: 'PUT', body: file })
+                    
+                })
+
+        }
+
+
+
+    }
+
     return (
         <>
             <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <p onClick={() => { setRoute('Home') }} className="f2 link dim black underline pa3 pointer">Go Back</p>
             </nav>
             <h1>Upload</h1>
-            {uploadState.success? <h1>Successful Upload</h1>: null}
-            <input onChange={handleUpload} type="file"/>
+            {uploadState.success ? <h1>Successful Upload</h1> : null}
+            <input onChange={(evt) => handleUploadChange(evt)} type="file" />
+            <button onClick={(evt) => handleUpload()}>UPLOAD</button>
 
         </>
     )
